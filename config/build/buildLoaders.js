17cf6cc095afe79c0c43afc66c8581cc9591da52
Danil <mydogfromhell666@gmail.com>
2023-07-29 04:28:38 +0300

    add basic webpack config

---
+++ config/build/buildLoaders.js
@@ -0,0 +1,53 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default function buildLoaders({isDev}) {
+
+	const svgLoader = {
+		test: /\.svg$/,
+		use: ['@svgr/webpack'],
+	};
+
+	const babelLoader = {
+		test: /\.(js|jsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+			}
+		}
+	};
+
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif)$/i,
+		use: [
+			{
+				loader: 'file-loader',
+			},
+		],
+	};
+
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {auto: (resPath) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev
+							? '[path][name]__[local]--[hash:base64:8]'
+							: '[hash:base64:8]',}
+				},
+			},
+			'sass-loader',
+		],
+	};
+
+	return [
+		fileLoader,
+		svgLoader,
+		babelLoader,
+		cssLoader
+	];
+}
