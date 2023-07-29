667d2f81227b71c1c57bddef8fa1a4e79cd3f7e4
Danil <mydogfromhell666@gmail.com>
2023-07-30 01:18:09 +0300

    add: prettier dep/script

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,26 +1,26 @@
-import React, {useId} from 'react';
-import {useGetAllQuery} from '../store';
+import React, { useId } from 'react';
+import { useGetAllQuery } from '../store';
 import './styles/global.scss';
-import {MissionArticle} from '../components/MissionArticle';
-import {Container} from '../layout/Container';
-import {Layout} from '../layout/Layout';
-import {Grid} from '../layout/Grid';
+import { MissionArticle } from '../components/MissionArticle';
+import { Container } from '../layout/Container';
+import { Layout } from '../layout/Layout';
+import { Grid } from '../layout/Grid';
 
 export const App = () => {
-
-	const {data = [], isLoading} = useGetAllQuery();
-
-	if (isLoading) return <h1>Loading...</h1>;
-
-	console.log(data);
+	const { data = [], isLoading } = useGetAllQuery();
 
 	return (
 		<Layout>
 			<Container>
 				<Grid>
-					{
-						data.map(mission => <MissionArticle key={mission.id} description={mission.details ?? 'details not found'} name={mission.name} date={new Date(mission.date_utc).toLocaleString('ru-RU')} />)
-					}
+					{data.map((mission) => (
+						<MissionArticle
+							key={mission.id}
+							description={mission.details ?? 'details not found'}
+							name={mission.name}
+							date={new Date(mission.date_utc).toLocaleString('ru-RU')}
+						/>
+					))}
 				</Grid>
 			</Container>
 		</Layout>
